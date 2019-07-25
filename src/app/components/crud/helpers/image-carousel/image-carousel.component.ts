0d8761354a4980858a8238811ecd1ea434468104
Viktor Andersson <Mtktwoo@gmail.com>
2019-07-25 16:58:18 +0200

    Added frame to image carousel

--- src/app/components/crud/helpers/image-carousel/image-carousel.component.ts
+++ src/app/components/crud/helpers/image-carousel/image-carousel.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ImageCarouselComponent implements OnInit {
 	images: Array<any> = [];
 	constructor() { 
-		this.images = [1].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+		this.images = [1,2,3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 	}
 
 	ngOnInit() {
